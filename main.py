80a21efd4f78d6a461df3f1f17e47aa81b661869
Max Tang <tw7613781@gmail.com>
2018-11-14 12:42:16 +0900

    feat: use pytrend lib to get data from google

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests
 import json
+from datetime import datetime, timedelta
+from pytrends.request import TrendReq
 def get_google_trend():
-    url = '''
-    https://trends.google.com/trends/api/widgetdata/multiline?hl=zh-CN&tz=-540&req=%7B%22time%22:%222018-11-12T08%5C%5C:01%5C%5C:42+2018-11-13T08%5C%5C:01%5C%5C:42%22,%22resolution%22:%22EIGHT_MINUTE%22,%22locale%22:%22zh-CN%22,%22comparisonItem%22:%5B%7B%22geo%22:%7B%7D,%22complexKeywordsRestriction%22:%7B%22keyword%22:%5B%7B%22type%22:%22BROAD%22,%22value%22:%22BTC+USD%22%7D%5D%7D%7D,%7B%22geo%22:%7B%7D,%22complexKeywordsRestriction%22:%7B%22keyword%22:%5B%7B%22type%22:%22BROAD%22,%22value%22:%22buy+bitcoin%22%7D%5D%7D%7D%5D,%22requestOptions%22:%7B%22property%22:%22%22,%22backend%22:%22CM%22,%22category%22:0%7D%7D&token=APP6_UEAAAAAW-vWZo26XLYFBBBQfdDOz29BzCyw27d0&tz=-540
-    '''
     try:
-        r = requests.get(url, timeout = 5)
-        data = json.loads(r.text[5:], encoding="utf-8")
-        return (data['default']['averages'])
+        pytrend = TrendReq(tz=-540)
+        pytrend.build_payload(kw_list=['BTC USD', 'buy bitcoin'], timeframe='now 1-d')
+        interest_over_time_df = pytrend.interest_over_time()
+        btc_usd_ave = interest_over_time_df['BTC USD'].mean()
+        buy_bitcoin_ave = interest_over_time_df['buy bitcoin'].mean()
+        return (btc_usd_ave, buy_bitcoin_ave)
     except Exception as e:
         print(f'Error when get_google_trend: {e}')
 
@@ -16,14 +18,23 @@ def get_krw_btc_from_upbit():
     try:
         r = requests.get(url, timeout = 5)
         data = json.loads(r.text, encoding="utf-8")
-        return (data[0]['opening_price'], data[0]['change_price'])
+        return (data[0]['opening_price'], data[0]['change_price'], data[0]['change_rate'], data[0]['high_price'], data[0]['low_price'])
     except Exception as e:
         print(f'Error when get_krw_btc_from_upbit: {e}')
 
-
 if __name__ == '__main__':
-    trend = get_google_trend()
-    message = f'BTC USD : buy bitcion = {trend}, rate is {trend[1] / trend[0]}'
+    # get google trend data of previous day with format (btc_usd_average, buy_bitcon_average)
+    rt = get_google_trend()
+    # get upbit exchange data with format (opening price, change price, change rate, high price, low price)
     price = get_krw_btc_from_upbit()
+    # calculate ratio of buy bitcon / btc usd
+    rate1 = rt[1] / rt[0]
+    # get ratio of price change rate compare with previous day
+    rate2 = price[2]
+    # strategy condition: ratio of buy bitcon / btc usd > 35% and price increase rate > 1%
+    if rate1 > 0.35 and rate2 > 0.01:
+        strategy = 'BUY'
+    else:
+        strategy = 'SELL'
+    message = f'BTC USD : buy bitcion = {rt}, rate is {rate1} and upbit BTC/KRW open price is {price[0]}, change price is {price[1]}, change rate is {price[2]}, today strategy is {strategy}, reference with yesterday high {price[3]}, yesterday low {price[4]}'
     print(message)
-    print(price)
