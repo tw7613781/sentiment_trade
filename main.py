00d03d98cb8b5e7f75a324bc8e4c7c3c69b92b94
Max Tang <tw7613781@gmail.com>
2018-11-15 10:45:37 +0900

    feat: add db to save history data

--- main.py
+++ main.py
@@ -2,12 +2,43 @@ import requests
 import json
 import config
 import logging
+import sqlite3
 from telethon import TelegramClient
-from datetime import datetime, timedelta
+from datetime import datetime
 from pytrends.request import TrendReq
 
 logging.basicConfig(format='%(name)s - %(asctime)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def db_init(cmd_sql):
+    conn = sqlite3.connect('history.db')
+    cursor = conn.cursor()
+    cursor.execute(cmd_sql)
+    cursor.close()
+    conn.close()
+
+
+def db_select(cmd_sql):
+    conn = sqlite3.connect('history.db')
+    cursor = conn.cursor()
+    cursor.execute(cmd_sql)
+    records = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    # return a 2-dimension array including sql select result
+    return records
+
+
+def db_insert(cmd_sql, params):
+    conn = sqlite3.connect('history.db')
+    cursor = conn.cursor()
+    cursor.execute(cmd_sql, params)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+
 def get_google_trend():
     try:
         pytrend = TrendReq(tz=-540)
@@ -24,7 +55,7 @@ def get_krw_btc_from_upbit():
     try:
         r = requests.get(url, timeout = 5)
         data = json.loads(r.text, encoding="utf-8")
-        return (data[0]['opening_price'], data[0]['change_price'], data[0]['change_rate'], data[0]['high_price'], data[0]['low_price'])
+        return data[0]['opening_price']
     except Exception as e:
         logger.error(f'Error when get_krw_btc_from_upbit: {e}')
 
@@ -38,19 +69,46 @@ def send(receiver, message):
 
 
 if __name__ == '__main__':
+    # init db
+    cmd = '''CREATE TABLE IF NOT EXISTS history
+                        (date varchar(20),
+                        bit_usd varchar(20),
+                        buy_bitcoin varchar(20),
+                        rate varchar(20),
+                        open_price varchar(20),
+                        change_rate varchar(20),
+                        strategy varchar(20))
+                    '''
+    db_init(cmd)
     # get google trend data of previous day with format (btc_usd_average, buy_bitcon_average)
-    rt = get_google_trend()
-    # get upbit exchange data with format (opening price, change price, change rate, high price, low price)
+    gtrend = get_google_trend()
+    # get today's opening price from exchage Upbit
     price = get_krw_btc_from_upbit()
     # calculate ratio of buy bitcon / btc usd
-    rate1 = rt[1] / rt[0]
-    # get ratio of price change rate compare with previous day
-    rate2 = price[2]
+    rate_gtrend = gtrend[1] / gtrend[0]
+    # get opening price of yesterday from db
+    cmd = 'SELECT open_price FROM history ORDER BY date DESC LIMIT 1'
+    st = db_select(cmd)
+    # no yesterday's data
+    if len(st) == 0:
+        price_yesterday = 0
+        price_diff = 0
+        rate_price = 0
+    else:
+        price_yesterday = st[0][0]
+        price_diff = float(price) - float(price_yesterday)
+        rate_price = price_diff / float(price_yesterday)            
     # strategy condition: ratio of buy bitcon / btc usd > 35% and price increase rate > 1%
-    if rate1 > 0.35 and rate2 > 0.01:
+    if rate_gtrend > 0.35 and rate_price > 0.01:
         strategy = 'BUY'
     else:
         strategy = 'SELL'
-    message = f'BTC USD : buy bitcion = {rt}, rate is {rate1} and upbit BTC/KRW open price is {price[0]}, change price is {price[1]}, change rate is {price[2]}, today strategy is {strategy}, reference with yesterday high {price[3]}, yesterday low {price[4]}'
+    # save to db
+    cmd = 'INSERT INTO history VALUES (?,?,?,?,?,?,?)'
+    date = datetime.now().strftime("%Y-%m-%d")
+    params = (date, gtrend[0], gtrend[1], rate_gtrend, price, rate_price, strategy)
+    db_insert(cmd, params)
+    # send to telegram
+    message = f'BTC USD : buy bitcoin = {gtrend}, rate is {rate_gtrend} and Upbit BTC/KRW open price is {price}, change price is {price_diff}, change rate is {rate_price}, today strategy is {strategy}'
     logger.info(message)
     send('me', message)
